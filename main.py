22547ad6a70fad0b36b1d946fc4fc964833c02c1
Bogdan <srk.bogdan@gmail.com>
2019-06-15 18:20:32 +0300

    add video

--- main.py
+++ main.py
@@ -7,26 +7,23 @@ token = 'E16A089A586A48418453B14787DF6F1C'
 sensifai_api = SensifaiApi(token)
 
 
-# files_list = ['/Users/bogdan/PycharmProjects/sensifai-test/video-sample-2.mp4']
+# files_list = [
+#     '/Users/bogdan/PycharmProjects/sensifai-test/videos/10 sec New York.mp4',
+#     '/Users/bogdan/PycharmProjects/sensifai-test/videos/video-sample-2.mp4'
+# ]
 # task_dict = sensifai_api.start_model(files=files_list)
-#
-#
 # print(json.dumps(task_dict, indent = 4))
-#
-# # for id in task_dict["succeed"]:
-# #     result = sensifai_api.get_result(id["taskId"])
-# #     pprint(id["file"])
-# #     JSON_FILE_PATH = "/Users/bogdan/PycharmProjects/sensifai-test/{}-result.json".format(id["file"])
-# #     json.dump(result, open(JSON_FILE_PATH, 'w'))
-# #     pprint(result)
-# #     pprint("_____________________________")
+
 
 task_dict = {
     "succeed": [
         {
-
+            "file": "10 sec New York.mp4",
+            "taskId": "81a3b8a7-4659-4865-b1b1-fe7fe05c4fb0"
+        },
+        {
             "file": "video-sample-2.mp4",
-            "taskId": "85abc56c-4d57-408b-99cd-7845bd65bca8"
+            "taskId": "ec896b66-a388-42e8-b5d8-c478f44b8245"
         }
     ],
     "cannotUpload": []
